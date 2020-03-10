6fc774e2d9b9d47006da4002ce03e74f8ad74260
Andrey <andrey.makarenok01@gmail.com>
2020-03-10 21:07:08 +0300

    CameraMover

---
+++ Assets/Scripts/Camera/CameraMovementBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraMovementBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private Transform _target = null;
+    [SerializeField] private Vector3 _offset = Vector3.zero;
+    [SerializeField] private float _movementSpeed = 5.0f;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position + _offset, _movementSpeed * Time.deltaTime);
+    }
+
+}
